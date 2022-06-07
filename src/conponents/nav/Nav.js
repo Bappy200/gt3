a1f9d7515c1590ea40770ba14683ef2b3334fedc
Bappy200 <sbappyi200@gmail.com>
2022-06-07 16:57:45 +0600

    header section done

--- src/conponents/nav/Nav.js
+++ src/conponents/nav/Nav.js
@@ -1,8 +1,53 @@
-import React from 'react'
-
+import { faNavicon, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react'
+import logo from '../../assets/logo.svg'
+import './nav.css'
 function Nav() {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div>Nav</div>
+    <div className='gpt__navbar'>
+        <div className='gpt__navbar__links'>
+            <div className='gpt__navbar_links_img'>
+              <img src={logo} alt="logo"/>
+            </div>
+            <div className='gpt__navbar_container'>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#wgpt">What is gpt?</a></li>
+                    <li><a href="#possibility">Open AI</a></li>
+                    <li><a href="#features">Case Studies</a></li>
+                    <li><a href="#blog">Library</a></li>
+                </ul>
+            </div>
+        </div>
+        <div className='gpt__sign'>
+          <a href='#signup'>sign in</a>
+          <button type='button'>sign up</button>
+        </div>
+        <div className='gpt__navbar__menu'>
+        {toggleMenu ? <FontAwesomeIcon icon={faXmark} onClick={()=> setToggleMenu(false)}/> :  <FontAwesomeIcon icon={faNavicon} onClick={()=> setToggleMenu(true)}/> }
+          {
+            toggleMenu && (
+              <div className='gpt__navbar__menu__container scale-up-center'>
+                <div className='gpt__navbar__menu__container__links'>
+                      <ul>
+                          <li><a href="#home">Home</a></li>
+                          <li><a href="#wgpt">What is gpt?</a></li>
+                          <li><a href="#possibility">Open AI</a></li>
+                          <li><a href="#features">Case Studies</a></li>
+                          <li><a href="#blog">Library</a></li>
+                      </ul>
+                  </div>
+                  <div className="gpt__navbar-menu_container-links-sign">
+                      <a href='#signup'>Signup</a>
+                      <button type="button">Sign in</button>
+                  </div>
+              </div>
+            )
+          }
+        </div>
+    </div>
   )
 }
 
--- src/container/header/Header.js
+++ src/container/header/Header.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import people from '../../assets/people.png'
+import AI from '../../assets/ai.png'
+import './header.css'
 function Header() {
   return (
-    <div>Header</div>
+    <section className='gpt__header section__padding'>
+        <div className='gpt__header__content'>
+              <h1 className='gradient__text'>Lets Build Something Amazing With GTP-3 OpenAI</h1>
+              <p>Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+              <div className='gpt__header__input'>
+                    <input placeholder='enter your email'/>
+                    <button type='button'>get start</button>
+              </div>
+              <div className='gpt__header__people'>
+                  <img src={people} alt="people"/>
+                  <p>Lorem ipsum is a placeholder</p>
+              </div>
+        </div>
+        <div className='gpt__header__image'>
+          <img src={AI} alt="ai"/>
+        </div>
+    </section>
   )
 }
 
